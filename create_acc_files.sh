82f1febaf365f8e2eefba6773dd48fc23d25c8da
Harry D <55989298+hd1117@users.noreply.github.com>
2021-06-03 18:13:46 +0100

    Delete create_acc_files.sh

--- create_acc_files.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-lin_acc_file = Rugby/Francis_Sailii_combined_300421.lac
-rot_acc_file = Rugby/Francis_Sailii_combined_300421.aac
-cog_id       = 80000000
-
-python3 madymo_2_k.py lin_acc_file rot_acc_file cog_id
