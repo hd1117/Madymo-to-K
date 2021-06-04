a5a5bb844e656636b2f712c740a39431d261ba6d
Harry D <55989298+hd1117@users.noreply.github.com>
2021-06-04 13:00:38 +0100

    Updated Variable Definition

--- create_acc_files.sh
+++ create_acc_files.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-lin_acc_file = Rugby/Francis_Sailii_combined_300421.lac
-rot_acc_file = Rugby/Francis_Sailii_combined_300421.aac
-cog_id       = 80000000
+lin_acc_file=Rugby/Francis_Sailii_combined_300421.lac
+rot_acc_file=Rugby/Francis_Sailii_combined_300421.aac
+cog_id=80000000
 
-python3 madymo_2_k.py lin_acc_file rot_acc_file cog_id
+python3 madymo_2_k.py ${lin_acc_file} ${rot_acc_file} ${cog_id}
